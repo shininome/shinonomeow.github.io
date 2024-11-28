4974245930b5b7ea42656a02e50eee2e988ec86a
東雲柊 <gtx2shino@gmail.com>
2024-11-28 13:33:10 +0800

    difference array

--- docs/.vuepress/nav/learn.js
+++ docs/.vuepress/nav/learn.js
@@ -1,7 +1,10 @@
 module.exports = {
-    text: "学习笔记",
-    link: "/learn/",
-    items: [
-        { text: "动手深度学习", link: "/learn/d2l/" },
-    ]
+  text: "学习笔记",
+  link: "/learn/",
+  items: [
+    { text: "动手深度学习", link: "/learn/d2l/" },
+    { text: "Algorithm", link: "/learn/algorithm/" },
+  ]
+
+
 };
